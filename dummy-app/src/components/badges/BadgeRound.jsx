ce11532d106c87961967b7591c7f678e26abeea5
Maria Dimitrijevic <maria.dimitrijevikj@gmail.com>
2020-01-15 14:03:10 +0800

    styled and positioned the round badge to look like the original one

--- dummy-app/src/components/badges/BadgeRound.jsx
+++ dummy-app/src/components/badges/BadgeRound.jsx
@@ -1,7 +1,23 @@
 import styled from "@emotion/styled";
 
 const BadgeRound = styled.div(props => ({
-  backgroundColor: props.theme.colors.primary
+  backgroundColor: props.theme.colors.primary,
+  borderRadius: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+  verticalAlign: 'center',
+  height: '14px',
+  width: '14px',
+  fontSize: '0.5em',
+  color: '#ffffff',
+  border: '2px solid white',
+  position: 'absolute',
+  top: '-1.3em',
+  left: '1.4em'
+
+
 }));
 
 export default BadgeRound;
